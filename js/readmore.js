db39783dd08165f9c78cb86b0fed0299dba3ee3e
tomlangbehn <tom.langbehn@posteo.de>
2019-12-01 19:59:08 +0100

    button readmore

--- js/readmore.js
+++ js/readmore.js
@@ -1,12 +1,24 @@
-function ShowHide(divId)
-{
-if(document.getElementById(divId).style.display == 'none')
-{
-document.getElementById(divId).style.display='block';
-}
-else
-{
-document.getElementById(divId).style.display = 'none';
-}
-}  
+// Add click event dynamically
+$(document).on("click", ".toggle-text-button", function() {
 
+  // Check if text is more or less
+  if ($(this).text() == "Read More") {
+
+    // Change link text
+    $(this).text("Read Less");
+    
+    // Travel up DOM tree to parent, then find any children with CLASS .toggle-text and slide down
+    $(this).parent().children(".toggle-text").slideDown();
+    
+  } else {
+
+
+    // Change link text
+    $(this).text("Read More");
+    
+    // Travel up DOM tree to parent, then find any children with CLASS .toggle-text and slide up 
+    $(this).parent().children(".toggle-text").slideUp();
+    
+  }
+  
+});
